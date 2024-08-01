4a591ef0ad2de6ed461aba5e086064c6fd4fec4f
amanbanjara <amansinghbanjara@gmail.com>
2024-08-01 21:39:33 +0530

    Portfolio using HTML, CSS, JAVASCRIPT
    Signed-off-by: amanbanjara <amansinghbanjara@gmail.com>

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+    for (const link of links) {
+        link.addEventListener('click', smoothScroll);
+    }
+});
+
+function smoothScroll(event) {
+    event.preventDefault();
+    const targetId = event.currentTarget.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    });
+}
